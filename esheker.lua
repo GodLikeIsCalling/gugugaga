39ce5a0e272ad412b86dc3bfc09536fa6179a274
GodLikeIsCalling <maksimghost.ru2010@gmail.com>
2025-05-06 15:12:00 +0300

    Update esheker.lua

--- esheker.lua
+++ esheker.lua
@@ -5,3 +5,7 @@ local Window = Library.CreateLib("dasda", "RJTheme3")
 local Tab = Window:NewTab("Farm")
   
 local Section = Tab:NewSection("Section Name")
+
+Section:NewSlider("SliderText", "SliderInfo", 500, 0, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
